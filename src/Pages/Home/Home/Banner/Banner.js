73a6812cf1c5d8cfd201c6fc57433fe7498bc22b
moinuddin <cpimoinuddin@gmail.com>
2022-11-07 15:33:59 +0600

    banner responsive adding

--- src/Pages/Home/Home/Banner/Banner.js
+++ src/Pages/Home/Home/Banner/Banner.js
@@ -13,7 +13,7 @@ const Banner = () => {
           <p>Welcome to  Creative Campus, one of the best software companies in Bangladesh. Welcome to a company where your ideas count, where your determination creates a new world, where your talent and hard-works are rewarded. At <span className='banner-span'>Creative Campus</span> &nbsp;we always endeavor to lead towards development and creating a flexible, dynamic and cooperative organizational cultural. <span className='banner-span'>Creative Campus</span> is one of the promising software, web application, mobile application &amp; game development companies of Bangladesh which aims at creating a new standard in the era of software technology.</p></div>
         
         <div className='col-md-6'>
-        <img className="banner-img" src={img1} alt="cartoon-1.png"  />
+        <img className="banner-img" src={img1} width="120px" height="120px" alt="cartoon-1.png"  />
         </div>
         <div className='row'>
         <div className='col-md-12'>
