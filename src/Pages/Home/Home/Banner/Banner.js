8082a3e032e7fe18f30c39083f61744f6ac33358
Md Moinuddin Kamal <cpimoinuddin@gmail.com>
2022-11-12 00:37:00 +0600

    design update

--- src/Pages/Home/Home/Banner/Banner.js
+++ src/Pages/Home/Home/Banner/Banner.js
@@ -1,36 +1,107 @@
 import React from 'react';
 import './Banner.css';
-import img1 from '.././../../Images/cartoon/cartoon-2.png';
+import img1 from '.././../../Images/cartoon/cartoon-1.png';
+import img2 from '.././../../Images/cartoon/cartoon-3.png';
+import img3 from '../../../Images/cartoon/cartoon-3.png';
+
 
 const Banner = () => {
     return (
     
-    <div className="container">
-      <div className="row">
-        <div className='col-md-6 banner-head'>
-        <h1>Advanced Features to Grow <br />Your Business</h1>
-        
-          <p>Welcome to  Creative Campus, one of the best software companies in Bangladesh. Welcome to a company where your ideas count, where your determination creates a new world, where your talent and hard-works are rewarded. At <span className='banner-span'>Creative Campus</span> &nbsp;we always endeavor to lead towards development and creating a flexible, dynamic and cooperative organizational cultural. <span className='banner-span'>Creative Campus</span> is one of the promising software, web application, mobile application &amp; game development companies of Bangladesh which aims at creating a new standard in the era of software technology.</p></div>
-        
-        <div className='col-md-6'>
-        <img className="banner-img" src={img1} width="120px" height="120px" alt="cartoon-1.png"  />
-        </div>
-        <div className='row'>
-        <div className='col-md-12'>
+    <div classNameName="container">
+      
 
-        
-         <div className='banner-button'>
-          <div className="hire-us-btn1">
-            <a href="#">Hire From Us</a>
-          </div>
-          <div className="contact-with-us-btn1">
-            <a href="#">Contact With Us</a>
-          </div>  
-          </div>  
-          </div>
-        </div>  
+<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+        {/* <div class="carousel-item active  ">
+            <div class="row p-5 mb-2  text-white d-flex align-items-center">
+                <div class="col-lg-6 ">
+                    <h1 className='text-white'> Quality Software Development</h1>
+                    <p className="text-white">This is the best headphone in the world for people who just want to waste time in front of funky world.
+
+                    </p>
+                        <h1 class="text-danger" >$420</h1>
+                        <button class="panda-btn-buynow">Buy Now </button>
+                </div>
+                <div class="col-lg-6">
+                    <img src={img1} class="d-block w-100" alt="..."/>
+                </div>
+            </div>
+        </div> */}
+
+        <div class="carousel-item active">
+
+            <div class="row p-5 mb-2  text-dark d-flex align-items-center">
+                <div class="col-lg-6 ">
+                    <h1 className='text-white'>World Wide Demand Course Sale
+                    </h1>
+                    <p className='text-white'>This is the best tv in the world for people who just want to waste time in front of tv.
+
+                        </p>
+                      
+                        <button class="panda-btn-buynow">Buy Now </button>
+                </div>
+                <div class="col-lg-6">
+                    <img src={img2} class="d-block w-100" alt="..."/>
+                </div>
+            </div>
         </div>
+        <div class="carousel-item">
+            <div class="row p-5 mb-2  text-dark d-flex align-items-center">
+                <div class="col-lg-6 ">
+                    <h1 className="text-white">Best IT Course</h1>
+                    <p className='text-white'>This is the best x-box in the world for people who just want to waste time in front of fake sports</p>
+                       
+                        <button class="panda-btn-buynow">Buy Now </button>
+                </div>
+                <div class="col-lg-6">
+                    <img src={img3} class="d-block w-100" alt="..."/>
+                </div>
+            </div>
         </div>
+        <div class="carousel-item">
+            <div class="row p-5 mb-2  text-dark d-flex align-items-center">
+                <div class="col-lg-6 ">
+                    <h1 className="text-white">Best IT Course</h1>
+                    <p className='text-white'>This is the best x-box in the world for people who just want to waste time in front of fake sports</p>
+                        
+                        <button class="panda-btn-buynow">Buy Now </button>
+                </div>
+                <div class="col-lg-6">
+                    <img src={img3} class="d-block w-100" alt="..."/>
+                </div>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <div class="row p-5 mb-2  text-dark d-flex align-items-center">
+                <div class="col-lg-6 ">
+                    <h1 className="text-white">Best IT Course</h1>
+                    <p className='text-white'>This is the best x-box in the world for people who just want to waste time in front of fake sports</p>
+                     
+                        <button class="panda-btn-buynow">Buy Now </button>
+                </div>
+                <div class="col-lg-6">
+                    <img src={img3} class="d-block w-100" alt="..."/>
+                </div>
+            </div>
+        </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
+        data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
+        data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+</div>
+
+    
+  </div>
+
+    
        
      
     
--- src/Pages/Promo/Promo.js
+++ src/Pages/Promo/Promo.js
@@ -1,47 +1,59 @@
 import React from 'react';
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// // import {  icon } from '@fortawesome/fontawesome-svg-core/import.macro' // <-- import styles to be used
+// import {  icon } from '@fortawesome/fontawesome-svg-core/import.macro' // <-- import styles to be used
+// import img1 from '../Images/img1.svg';
 import './Promo.css';
 
 const Promo = () => {
     return (
         <div className='container'>
-         <div className='row'>
-        <div className='col promo-header'>
-          
-        <h1>We Help You Stay Connected</h1>
-        <p>Dynamically initiate market positioning total linkage with clicks-and-mortar technology procrastinate compelling data for markets.</p>
+        <div className='row'>
+          <div className='col-md-12'>
+            <h1 className='text-white text-center'>We Help You Stay Connected</h1>
+            <p className='text-white text-center m-5 p-4 w-50'>Dynamically initiate market positioning total linkage with clicks-and-mortar technology procrastinate compelling data for markets.</p>
+          </div>
         </div>
-     </div>
-   
-    <div className='row'>
-      <div className='col-md-4'>
-      <div className="card-secton ">
-        <div className="stay-c">
-        <i class="fa-solid fa-wifi"></i>
-        <h2>Stay Connected</h2>
-          <p>We are always at your service. Stay tuned with absolute ideas to get technical support on any topic.</p>
-        </div>
+        <div class="row row-cols-1 row-cols-md-3 g-4 shadow-lg p-3 mb-5  rounded">
+  <div class="col ">
+    <div class="card h-100 bg-card mb-5 p-3 border p-3 mb-2 rounded-2  d-flex align-items-center justify-content-between">
+      <img src={img1} class="card-img-top w-25 pt-4 text-white" alt="..."/>
+      <div class="card-body">
+        <h5 class="card-title text-center text-white ">Card title</h5>
+        <p class="card-text text-center p-3 text-white">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100 bg-card-mid mb-5 p-3 border p-3 mb-2 rounded-2  d-flex align-items-center justify-content-between">
+      {/* <img src="..." class="card-img-top" alt="..."/> */}
+      <svg viewBox="0 0 10 10" xmlns="https://iconscout.com/lottie/web-builder-6564577">
+  <rect width="10" height="10">
+    <animate
+      attributeName="rx"
+      values="0;5;0"
+      dur="10s"
+      repeatCount="indefinite" />
+  </rect>
+</svg>
+      <div class="card-body">
+        <h5 class="card-title text-center  text-white">Card title</h5>
+        <p class="card-text text-center p-3 text-white">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100 bg-card-right mb-5 p-3 border p-3 mb-2 rounded-2  d-flex align-items-center justify-content-between">
+      <img src="..." class="card-img-top" alt="..."/>
+      
+      <div class="card-body">
+        <h5 class="card-title text-center text-white ">Card title</h5>
+        <p class="card-text text-center text-white">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  
+</div>
 
-      </div>
-      </div>
-   
-    
-      <div className='col-md-4'>
-        <div className="ep">
-        <i class="fa-solid fa-users"></i>
-          <h2>Encourage Participation</h2>
-          <p>We are always at your service. Stay tuned with absolute ideas to get technical support on any topic.</p>
-        </div>
-        </div>
-        <div className='col-md-4'>
-        <div className="bc">
-        <i class="fa-solid fa-bezier-curve"></i>
-          <h2>Build Connections</h2>
-          <p>We are always at your service. Stay tuned with absolute ideas to get technical support on any topic.</p>
-        </div>
-        </div>
-        </div>
         </div>
    
       
